aeb8df5ff683331de884e2f6d56db5cba7f1a571
Stif <stifskere@gmail.com>
2023-07-04 21:14:56 +0200

    someone help me.

--- Commands/Info.cs
+++ Commands/Info.cs
@@ -15,10 +15,11 @@ public class Info : InteractionModuleBase<SocketInteractionContext>
             .WithTitle("About this bot")
             .WithDescription(
                 "This bot is all about counting how racist you are! Whenever you say the NWord this bot will count it and make a percentile.")
-            .AddField("Don't worry", "This data is private unless you want to expose it ||or if you reach the top 5 most racist users in the server||")
+            .AddField("Don't worry", "This data is private unless you want to expose it ||or if you reach the top 5 most racist users||")
             .AddField("Current counted words", string.Join(", ", Bot.CountedWordTypes))
             .AddField("Disclaimer", "The data on this bot is private, and all exposition is done trough the commands you can see.")
             .AddField("Participation", "You can opt out of this bot anytime using the </opt out:1125541780855722066> command and opt in again using the </opt in:1125541780855722066> command.")
+            .AddField("Bot source", "This bot is fully open source and made 100% in C#, you can check it [here](https://github.com/stifskere/NWordCounter)")
             .WithColor(Bot.DefaultEmbedColor)
             .WithFooter("glhf -memw");
 
